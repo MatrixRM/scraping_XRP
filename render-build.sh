b09c81e4f1ae7ad39b05e742b9e7a8bbcbbd7b4e
Roberto Mânica <80008228+MatrixRM@users.noreply.github.com>
2024-11-29 16:34:24 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-# Instala o Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+# Baixa e adiciona a chave pública do Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+# Adiciona o repositório do Google Chrome
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+
+# Atualiza os pacotes e instala o Google Chrome
+apt-get update
+apt-get install -y google-chrome-stable
